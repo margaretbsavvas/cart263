93c10d7bb2a96aa06b670acaa50853cb3a5ed65a
margaretbsavvas <margaretbeaumontsavvas@gmail.com>
2021-01-27 15:29:56 -0500

    E1: adding a timer
    I've added a count down timer of 5 seconds.
    the game is over when the timer reaches  0

--- activity1-sausagedog/js/script.js
+++ activity1-sausagedog/js/script.js
@@ -10,6 +10,9 @@ let matchas = [];
 let wasabiImage = undefined;
 let wasabi = undefined;
 
+//adding a countdown timer to find the wasabi
+let timer = 5
+
 //"i" allows the program to read the image numbers
 function preload() {
   for (let i = 0; i < NUM_MATCHA_IMAGES; i++) {
@@ -39,11 +42,24 @@ function setup() {
 function draw() {
   background(131, 181, 129);
 
+
+ //
   for (let i = 0; i < matchas.length; i++){
     matchas[i]. update();
   }
 
   wasabi.update();
+
+  textAlign (CENTER, CENTER);
+  textSize(100);
+  text(timer, width/2, height/2);
+
+  if (frameCount % 60 == 0 && timer > 0) {
+    timer --;
+  }
+  if (timer == 0) {
+    text("WHY IS MY ICE CREAM BURNING ME", width/2, height*0.7);
+  }
 }
   function mousePressed(){
     wasabi.mousePressed();
