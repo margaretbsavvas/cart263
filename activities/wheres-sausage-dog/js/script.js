b349671f75f45ed861fdcad1d014b4700dc701af
margaretbsavvas <margaretbeaumontsavvas@gmail.com>
2022-01-20 19:11:13 -0500

    step 3
    Adding the animals to the program

--- activities/wheres-sausage-dog/js/script.js
+++ activities/wheres-sausage-dog/js/script.js
@@ -7,27 +7,39 @@ author, and this description to match your project!
 */
 
 "use strict";
+//declaring global constants for number of animal images and number of animals to display
+const NUM_ANIMAL_IMAGES = 10;
+const NUM_ANIMALS = 100;
 
+//arrays for animal images and for animal objects
+let animalImages = [];
+let animals= [];
 
-/**
-Description of preload
-*/
 function preload() {
-
+//creating a loop to load all animal images by using the iterator
+  for (let i = 0; i <NUM_ANIMALS_IMAGES; i++){
+    let animalImage =loadImage(`assets/images/animal$${i}.png`);
+    animalImages.push(animalImage);
+  }
 }
 
-
-/**
-Description of setup
-*/
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  //create the animal
+  for (let i = 0; i < NUM_ANIMALS; i++) {
+    let x = random (0, width);
+    let y = random (0, height);
+    let animalImage = random(animalImages);
+    let animal = new animal(x, y, animalImage);
+    animals.push(animal);
+  }
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
-
+  background(255, 255, 0);
+//using a for loop to go through animals and call the update
+  for (let i = 0; i < animals.length; i++) {
+   animals[i].update();
+  }
 }
