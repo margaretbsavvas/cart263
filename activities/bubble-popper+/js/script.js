d0fa290fc5b891ae53a4371e1114518ac7787d3b
margaretbsavvas <margaretbeaumontsavvas@gmail.com>
2022-02-11 13:13:22 -0500

    Setting up file for Bubble Popper +

---
+++ activities/bubble-popper+/js/script.js
@@ -0,0 +1,32 @@
+/**
+Bubble Popper +
+Margaret Beaumont-Savvas
+
+Popping bubbles with your index finger as a pin!
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
