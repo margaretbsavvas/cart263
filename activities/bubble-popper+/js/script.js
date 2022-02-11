6e42a7af9661b2909265930c47bfb4950faba249
margaretbsavvas <margaretbeaumontsavvas@gmail.com>
2022-02-11 13:52:28 -0500

    step 1

--- activities/bubble-popper+/js/script.js
+++ activities/bubble-popper+/js/script.js
@@ -7,6 +7,14 @@ Popping bubbles with your index finger as a pin!
 
 "use strict";
 
+//declaring a video variable for access to webcam
+let video = undefined;
+
+//declaring variable for handpose
+let handpose = undefined;
+
+//current set of predictions
+let predictions = [];
 
 /**
 Description of preload
@@ -16,12 +24,29 @@ function preload() {
 }
 
 
-/**
-Description of setup
-*/
+//setting up canvas
 function setup() {
+  createCanvas(640, 480);
+
+  //access to user's webcam
+  video = createCapture(VIDEO);
+  video.hide();
+
+  //loading handpose model
+  handpose = ml5.handpose(video, {
+    fliphorizontal: true
+  }, function() {
+    console.log(`Model loaded.`);
+  });
+
+  //listen for predictions
+  handpose.on(`predict`, fucntion(results) {
+    console.log(results);
+    predictions = results;
+  });
 
 }
+
 
 
 /**
